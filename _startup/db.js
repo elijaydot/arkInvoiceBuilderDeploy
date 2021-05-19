2f9a6390d7aedcdafee26e41767e0cd691473183
Pranadot <pranadot@gmail.com>
2021-05-19 12:43:48 +0100

    updated db.js and app.js files

--- _startup/db.js
+++ _startup/db.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
-// const config = require('config');
 
 module.exports = function () {
-  //   const db = config.get('db');
-  // const db =
-  //   "mongodb://" +
-  //   process.env.db_USER +
-  //   ":" +
-  //   process.env.db_PW +
-  //   "@localhost/" +
-  //   process.env.db_NAME;
-  const db = "mongodb://localhost/" + process.env.db_NAME;
+  const db = process.env.MONGODB_URI;
 
   mongoose.set("useCreateIndex", true);
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useUnifiedTopology", true);
-  //   mongoose.connect(db)
-  //     .then(() => winston.info(`Connected to ${db}...`));
+
   mongoose
     .connect(db)
     .then(() => {
--- app.js
+++ app.js
@@ -1,52 +1,42 @@
 const express = require("express");
 const path = require("path");
-// const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
+// const logger = require("morgan");
+// const cors = require("cors");
 const createError = require("http-errors");
-require("dotenv").config();
 const app = express();
 
-const indexRouter = require("./_routes/index");
 const invoicesRouter = require("./_routes/invoices");
-
-const catchAllRouter = require("./_routes/catchall");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use(bodyParser.json());
-app.use(logger("tiny"));
+// app.use(logger("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// app.use("/images", express.static(path.join(__dirname, "_images")));
 
 // Serve UI as static files
-// app.use("/", express.static(path.join(__dirname, "public/dist/")));
+app.use("/", express.static(path.join(__dirname, "public/dist/")));
 
 // CORS not needed if UI is integrated
-app.use(cors());
+// app.use(cors());
 
 // Connect Database
 require("./_startup/db")();
 
 // Enable Production mode
-// require("./_startup/production")(app);
+require("./_startup/production")(app);
 
 // Server calls
 app.use("/invoices", invoicesRouter);
 
 // All other calls via Angular UI
-// app.use((req, res, next) => {
-//  res.sendFile(path.join(__dirname, "public/dist/", "index.html"));
-// });
-
-app.use("/", indexRouter);
-// app.use("**", catchAllRouter);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "public/dist/", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
